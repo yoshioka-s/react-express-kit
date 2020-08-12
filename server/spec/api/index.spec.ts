038378180c12d5d3894e2074b6ba8a738b8b7be7
yoshioka-s <yoshioka63@gmail.com>
2020-08-12 13:22:02 +0900

    fix spec

--- server/spec/api/index.spec.ts
+++ server/spec/api/index.spec.ts
@@ -1,13 +1,14 @@
 import "regenerator-runtime/runtime.js"
-// import request from 'supertest'
-const request = require('supertest')
+import request from 'supertest'
 import app from '../../app'
 
 describe('The greet', () => {
-  it('returns hello', async () => {
+  it('successes', async () => {
     const res = await request(app).get('/api/greet')
     expect(res.statusCode).toBe(200)
-    console.log(res);
+  })
+  it('returns "hello world"', async () => {
+    const res = await request(app).get('/api/greet')
     expect(res.body).toBe('hello world')
   })
 })
